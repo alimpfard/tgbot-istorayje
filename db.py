88df11d488817225233c791975eb40cdcce8a90d
AnotherTest <ali.mpfard@gmail.com>
2019-08-08 12:46:54 +0430

    url

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
 class DB:
-    def __init__(self, addr='127.0.0.1', port=27017, name='istorayje'):
-        self.client = MongoClient(addr, port)
+    def __init__(self, url='mongodb://127.0.0.1' name='istorayje'):
+        self.client = MongoClient(url)
         self.db_name = name
         self.db = self.client[name]
     
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-bot = IstorayjeBot(os.environ['TOKEN'], DB())
+bot = IstorayjeBot(os.environ['TOKEN'], DB(url=os.environ['MONGO_URL']))
 
 mode = os.environ.get('MODE', 'dev')
 
