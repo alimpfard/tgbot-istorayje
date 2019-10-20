5b8d7fe97f93a978bee2bc54c1f22bdde3a809d4
AnotherTest <ali.mpfard@gmail.com>
2019-10-20 23:23:02 +0330

    ..add anilist

---
+++ anilist.py
@@ -0,0 +1,95 @@
+import json, requests
+from telegram import (
+    InlineQueryResultArticle, ParseMode, InputTextMessageContent
+)
+from uuid import uuid4
+
+class anilist:
+    url = 'https://graphql.anilist.co'
+    
+    @classmethod
+    def query(qry: str, vars: dict):
+        return requests.post(anilist.url, json={'query': qry, 'variables': vars}).json()
+    
+    @classmethod
+    def squery_render(terms: str):
+        media = anilist.simple_query(terms)['data']['Page']['media']
+        responses = [
+            InlineQueryResultArticle(
+                id=uuid4(),
+                title=f"{'R' if len(media) else 'There were no r'}esults for query '{terms}'",
+                input_message_content=InputTextMessageContent('Y u clickin\' this?')
+            )
+        ]
+        def timefmt(t):
+            if not t:
+                return '???'
+            return f'{t} seconds'
+        def nextEpisode(episodes: list):
+            eps, time = None, None
+            for ex in episodes:
+                if ex['timeUntilAiring'] > 0:
+                    eps = ex['episode']
+                    time = ex['timeUntilAiring']
+                    break
+            return f"{eps} in {timefmt(time)}"
+
+        for m in media:
+            responses.append(
+                InlineQueryResultArticle(
+                    id=uuid4(),
+                    title=(lambda t: f"{'[🌶] ' if m['isAdult'] else ''}{t['english'] or m['romaji']} ({m['native']})")(m['title']),
+                    thumb_url=m['coverImage']['medium'],
+                    input_message_content=InputTextMessageContent(
+                        (f"<b>{m['title']['english'] or m['title']['romaji']} ({m['startDate']['year']})</b>\n" +
+                         f"Original name: {m['title']['native']}\n" +
+                         f"Status: {m['status']}\n" +
+                         f"Genres: {', '.join(m['genres'])}\n" +
+                         f"Total episode count: {m['episodes']}\n" +
+                         (f"Next episode: {nextEpisode(m['airingSchedule'])}\n" if m['status'] == 'AIRING' else '') +
+                         f"Cover image: {m['coverImage']['large']}"
+                        )
+                    ),
+                    reply_markup='HTML'
+                )
+            )
+        return responses
+    @classmethod
+    def simple_query(terms: str):
+        return anilist.query(
+            '''
+            query($id: Int, $page: Int, $perPage: Int, $search: String) {
+                Page (page: $page, perPage: $perPage) {
+                    media(search: $search) {
+                        id
+                        title {
+                            romaji
+                            native
+                            english
+                        }
+                        type
+                        format
+                        status
+                        description
+                        season
+                        startDate { year }
+                        episodes
+                        duration
+                        coverImage {
+                            medium
+                            large
+                        }
+                        genres
+                        isAdult
+                        airingSchedule {
+                            nodes {
+                                timeUntilAiring
+                                episode
+                            }
+                        }
+                    }
+                }
+            }
+            ''',
+            dict(search=terms, page=1, perPage=5)
+        )
